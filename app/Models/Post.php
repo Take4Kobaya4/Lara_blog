7c4ff99a6cf56227ae9b63615fc3e2fcb8afa096
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2024-05-22 23:14:22 +0900

    カテゴリと投稿のリレーション対応

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,16 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\User;
+use App\Models\Category;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'title', 'slug', 'excerpt', 'content', 'image'
+        'title', 'slug', 'excerpt', 'content', 'image', 'user_id', 'category_id'
     ];
 
     public function author(){
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// 投稿におけるCRUD処理のルートを設定するために設置
+use App\Http\Controllers\PostController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // ユーザーであれば、投稿できるようにルートを設定
+    Route::resource('/posts', PostController::class);
 });
 
 require __DIR__.'/auth.php';
