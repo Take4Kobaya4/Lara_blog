ea80ea967f9ee3e173b3d789d59aca1b3ac017d5
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2024-05-22 15:56:38 +0900

    RoleとUserのモデルにてリレーションの設定 - Role（単数） - User（複数）

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Models\Role;
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -43,5 +45,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role(){
+        return $this->belongsTo(Role::class);
     }
 }
